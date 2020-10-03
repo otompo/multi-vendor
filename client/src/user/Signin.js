54440a5291d9448c0870264358d09ca72d18a6b9
otompo <abagnasalifusasco@gmail.com>
2020-10-03 09:10:31 +0100

    Create Signin.js

---
+++ client/src/user/Signin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../core/Layout'
+
+const Signin = () => {
+    return (
+        <div>
+            <Layout title="Signin Page" description="Node React E-commerce App">
+                    ......
+            </Layout>
+           
+       </div>
+    );
+}
+
+export default Signin;
